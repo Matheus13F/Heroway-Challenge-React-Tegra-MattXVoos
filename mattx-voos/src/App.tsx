af2a902139405def5dd49e0f876d708111fcbef3
matheus13f <matheus-goes13@hotmail.com>
2020-03-01 23:26:12 -0300

    adicionado todas as funcionalidades, cards agora retornam todas as informações

--- mattx-voos/src/App.tsx
+++ mattx-voos/src/App.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useDebugValue } from 'react';
 import './CSS/index.css';
-import GetCard from "./Components/Cards/Card";
 import aviao from './IMG/aviao.svg';
 import aviaodestino from './IMG/aviaodestino.svg';
 import aviaoorigem from './IMG/aviaoorigem.svg';
@@ -9,18 +8,9 @@ import timetravel from './IMG/timetravel.svg';
 import airdate from './IMG/airdate.svg';
 import heroway from './IMG/heroway.svg';
 import conection from './IMG/conection.svg';
-import { debuglog } from 'util';
+
 
 function App() {
-
-  // interface IVoos {
-  //   chegada: String;
-  //   data_saida: String;
-  //   destino: String;
-  //   origem: String;
-  //   valor: Number;
-  //   voo: String;
-  // }
 
   const [listaVoos, setListaVoos] = React.useState([]);
 
@@ -48,7 +38,7 @@ function App() {
         },
         body: JSON.stringify(body)
       })
-      debugger;
+      ;
       const todosvoos = await responseVoos.json();
       console.log(todosvoos);
       setAirpotList(todosvoos);
@@ -76,16 +66,13 @@ function App() {
   const [from, setFrom] = useState(String);
   const [to, setTo] = useState(String);
   const [date, setDate] = useState(String);
+  const [temp, setTemp] = useState([]);
   
   async function getAirport() {
-    try {
       const response = await fetch('https://api-voadora.dev.tegra.com.br/flight/companies');
       const listAirport = await response.json();
       setAirpotList(listAirport);
-
-    } catch (err) {
-      alert('A conexão foi perdida :( \n    - Reload F5');
-    }
+      setTemp(listAirport);
   }
 
   useEffect(() => { getAirport() }, []);
@@ -99,25 +86,25 @@ function App() {
           <p className="matt">Matt-<span className="corX">X</span> Voos  <img src={aviao} className="sizeImg" alt="aeroplane" /></p>
           <h1>Passagens Aéreas de um jeito fácil!!</h1>
           <div className="linha"></div>
-        </div>    
+        </div>
 
         <div className="search-bar">
           <span className="description">Aeroporto - Origem</span>
 
           <select name="partida" id="partida" onChange={handlePartida}>
-            {airportList.map(airport => 
-              <option key={airport.nome} value={airport.aeroporto}>  
-                {airport.nome} - {airport.aeroporto} - {airport.cidade} 
+            {airportList.map(airport =>
+              <option key={airport.nome} value={airport.aeroporto}>
+                {airport.nome} - {airport.aeroporto} - {airport.cidade}
               </option>)}
           </select>
-          
+
           <span className="description">Aeroporto - Destino</span>
 
-        <select name="destino" id="destino" onChange={handleDestino}>
-            {airportList.map(airport => 
-            <option key={airport.nome} value={airport.aeroporto}>
-                {airport.nome} - {airport.aeroporto} - {airport.cidade}  
-            </option>)}
+          <select name="destino" id="destino" onChange={handleDestino}>
+            {airportList.map(airport =>
+              <option key={airport.nome} value={airport.aeroporto}>
+                {airport.nome} - {airport.aeroporto} - {airport.cidade}
+              </option>)}
           </select>
 
           <span className="description">Data da Viagem (Apenas entre 10 e 18 de FEV de 2019)</span>
@@ -129,48 +116,65 @@ function App() {
         <div className="rodape">
           <h3 className="rodape-text">Copyright © Desafio Tegra 2020 - Matheus ("MattX") Ferreira</h3>
         </div>
-      </section>        
-        {/* <!-- CONTEUDO DO SITE --> */}
-              
+      </section>     
+
+        {/* <!-- Card de resultado de viagem --> */}              
       <section>
         <div className="container">
           {listaVoos.map((voos) => {
-                debugger;
-                return (
-                  <div className="Viagem1">
-                    <div className="LinhaCard">
-                      <img src={aviaoorigem} className="sizeCard" alt="logo" /><h4>Aeroporto de Origem: {voos.origem} </h4>
-                    </div>
-                    <div className="LinhaCard">
-                      <img src={aviaodestino} className="sizeCard" alt="logo" /><h4>Aeroporto de Destino: {voos.destino} </h4>
-                    </div>
-                    <div className="LinhaCard">
-                      <img src={timetravel} className="sizeCard1" alt="logo" /><h4>Tempo de Voo: </h4>
-                    </div>
-                    <div className="LinhaCard">
-                      <img src={airdate} className="sizeCard1" alt="logo" /><h4>Data do Voo: {voos.date} </h4>
-                    </div>
-                    <div className="LinhaCard">
-                      <img src={money} className="sizeCard2" alt="logo" /><h4>Valor da Passagem: </h4>
-                    </div>
-                    <div className="LinhaCard">
-                      <img src={conection} className="sizeCard2" alt="logo" /><h4>Escalas: </h4>
-                    </div>
-                    <img src={heroway} className="sizeImg2" alt="aeroplane" />
-                  </div>
-                );
+            let valor = 0;
+            let tempovoo = 0;
+            let escala = "";
+            let count = 0;
+
+            voos.voos.map((voosarray) => {
+
+              valor += voosarray.valor;
+              let horariochegada, horariosaida, tempofinal;
+              horariosaida = new Date(voosarray.data_saida + " " + voosarray.saida).getTime();
+              horariochegada = new Date(voosarray.data_saida + " " + voosarray.chegada).getTime();
+              tempofinal = horariochegada - horariosaida;
+              tempovoo += (tempofinal / (1000 * 60 * 60));
+              const cidadeorigem = temp.find((x) => {
+                return x.aeroporto == voosarray.origem;
+              })
+              const cidadedestino = temp.find((x) => {
+                return x.aeroporto == voosarray.destino;
+              })
+
+              escala += count == 0 ? (cidadeorigem.cidade + " >> " + cidadedestino.cidade) : (" >> " + cidadedestino.cidade);
+              count++;
+
+            });
+            
+            return (
+              <div className="Viagem1">
+                <div className="LinhaCard">
+                  <img src={aviaoorigem} className="sizeCard" alt="logo" /><h4>Aeroporto de Origem: {voos.origem} </h4>
+                </div>
+                <div className="LinhaCard">
+                  <img src={aviaodestino} className="sizeCard" alt="logo" /><h4>Aeroporto de Destino: {voos.destino} </h4>
+                </div>
+                <div className="LinhaCard">
+                  <img src={timetravel} className="sizeCard1" alt="logo" /><h4>Tempo de Voo: {tempovoo.toFixed(2).replace(".", ":")} horas</h4>
+                </div>
+                <div className="LinhaCard">
+                  <img src={airdate} className="sizeCard1" alt="logo" /><h4>Data do Voo: {voos.date} </h4>
+                </div>
+                <div className="LinhaCard">
+                  <img src={money} className="sizeCard2" alt="logo" /><h4>Valor da Passagem: R$ {valor.toLocaleString('pt-BR')} </h4>
+                </div>
+                <div className="LinhaCard">
+                  <img src={conection} className="sizeCard2" alt="logo" /><h4>Escalas: {escala} </h4>
+                </div>
+                <div className="LinhaHeroway">
+                  <img src={heroway} className="sizeImg2" alt="aeroplane" />
+                </div>
+              </div>              
+            );
           })}
 
-          {/* {listaVoos.map((setListaVoos) => { 
-            return(
-              <h4>{setListaVoos.chegada}</h4>
-            )
-          })} */}
-
-          {/* <GetCard /> */}
         </div>
-
-      
       </section>    
     </>
   );
